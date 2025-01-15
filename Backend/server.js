61c0420095c0597426689b1953a506b05f0b1dc1
kanav789 <kanavrana91@gmail.com>
2025-01-15 22:37:53 +0530

    implementing the server

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,17 @@
-console.log("first");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const Textspeech = require("./texttospeech");
+
+async function getGenerativeModel() {
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const prompt = "okey please stop";
+
+  const result = await model.generateContent(prompt);
+  Textspeech(result.response.text());
+}
+
+getGenerativeModel();
