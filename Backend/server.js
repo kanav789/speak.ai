c4b96eae59d1d8ef66d20d60199a7494a43d04db
kanav789 <kanavrana91@gmail.com>
2025-01-16 13:19:33 +0530

    index.ejs

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,31 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const dotenv = require("dotenv");
-
+const express = require("express");
+const path = require("path");
+const app = express();
+const bodyParser = require("body-parser");
 dotenv.config();
 const Textspeech = require("./texttospeech");
 
-async function getGenerativeModel() {
+async function getGenerativeModel(prompt) {
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-  const prompt = "okey please stop";
-
   const result = await model.generateContent(prompt);
   Textspeech(result.response.text());
+  console.log(result.response.text());
 }
+app.set("view engine", "ejs");
 
-getGenerativeModel();
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.post("/", (req, res) => {
+  const transcript = req.body.transcript;
+  getGenerativeModel(transcript);
+});
+app.listen(8080);
